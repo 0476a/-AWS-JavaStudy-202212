bd71f5fe4df1518b096c6778112341a88440375f
0476a <0476a@naver.com>
2023-01-16 20:04:37 +0900

    2023.01.16 8번째 커밋 (배열, 컬랙션 수정)

--- src/j12_배열/Array4.java
+++ src/j12_배열/Array4.java
@@ -4,6 +4,11 @@ public class Array4 {
 
 	public static void main(String[] args) {
 		J12_User[] users = new J12_User[3];
+		
+		users[0] = new J12_User("aaa", "1234", "김삼겹", "aaa@gmail.com");
+		users[1] = new J12_User("bbb", "1234", "박대패", "bbb@gmail.com");
+		users[2] = new J12_User("ccc", "1234", "명갈비", "ccc@gmail.com");
+		
 		J12_User[] users2 = new J12_User[] {
 				new J12_User("aaa", "1234", "김삼겹", "aaa@gmail.com"),
 				new J12_User("bbb", "1234", "박대패", "bbb@gmail.com"),
@@ -15,9 +20,7 @@ public class Array4 {
 				new J12_User("ccc", "1234", "명갈비", "ccc@gmail.com")
 		};
 
-		users[0] = new J12_User("aaa", "1234", "김삼겹", "aaa@gmail.com");
-		users[1] = new J12_User("bbb", "1234", "박대패", "bbb@gmail.com");
-		users[2] = new J12_User("ccc", "1234", "명갈비", "ccc@gmail.com");
+		
 
 		for(int i = 0; i < users.length; i++) {
 			
--- src/j19_컬렉션/Student.java
+++ src/j19_컬렉션/Student.java
@@ -2,10 +2,6 @@ package j19_컬렉션;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
 
 //@NoArgsConstructor // 기본 생성자
 //@RequiredArgsConstructor 
@@ -17,10 +13,11 @@ import lombok.ToString;
 
 @AllArgsConstructor // 전체 생성자
 @Data
-
 public class Student {
 	//@Getter // 변수위에 해주면 해당 변수의 getter가 생성
 	private String name; // 이름
 	private int year; // 학년
 //	private final String address;
+	
+	
 }
