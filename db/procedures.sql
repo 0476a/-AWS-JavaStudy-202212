aab01c4d7d5f901d23a2cd5a1a3cfd7730bda1ca
0476a <0476a@naver.com>
2023-02-01 17:52:10 +0900

    2023.02.01(db , 파이썬 추가) (파이썬, 람다 수정)

---
+++ db/procedures.sql
@@ -0,0 +1,7 @@
+call insert_product('아이패드 프로');
+
+/* 뷰 불러오기 */ 
+select
+	*
+from
+	library_view;
--- src/j24_람다/Lambda2.java
+++ src/j24_람다/Lambda2.java
@@ -10,7 +10,12 @@ import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Objects;
 import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
 import java.util.function.Supplier;
+import java.util.jar.Attributes.Name;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Lambda2 {
 	
@@ -102,6 +107,54 @@ public class Lambda2 {
 			System.out.println("value: " + entry.getValue());
 			System.out.println();
 		}
+		
+		// 4. Function<T , R> // 일회용으로 만들어서 사용하는 함수
+		
+		Function<String, Integer> h = num -> Integer.parseInt(num);
+		
+		int convertStrNum1 = h.apply("10000");
+		int convertStrNum2 = h.apply("20000");
+		
+		System.out.println(convertStrNum1 + convertStrNum2);
+		
+		// 5. Predicate<T>
+//		Predicate<String> p1 = str -> str.startsWith("김");
+//		Predicate<String> p2 = str -> str.startsWith("이");
+		
+//		System.out.println(p1.or(p2).test("이삼겹"));
+//		System.out.println(p1.and(p2).test("김삼겹"));
+		
+		Function<Predicate<String>, Boolean> function1 = 
+				
+				predicate -> predicate.or(str -> str.startsWith("이")).test("김삼겹");
+				
+		boolean rs = function1.apply(str -> str.startsWith("김"));
+		System.out.println(rs);
+		
+		List<String> nameList = new ArrayList<>();
+		nameList.add("김삼겹");
+		nameList.add("이무쇠");
+		nameList.add("박김치");
+		nameList.add("최무기");
+		
+		
+		// 스트림 -> 일회용
+		Stream<String> stream = nameList.stream().filter(name -> name.startsWith("김"));
+//		stream.forEach(name -> System.out.println(name));
+		List<String> newList = stream.collect(Collectors.toList());
+//		System.out.println(newList);
+		
+		newList.forEach(str -> System.out.println(str));
+		
+		System.out.println("========== 한줄로 표현 ==========");
+		
+		nameList.stream()
+			.filter(name -> name.startsWith("김"))
+			.collect(Collectors.toList())
+			.forEach(System.out::println);
+		//  .forEach(name -> System.out.println(name)); // 위에랑 똑같은 거임!
+		
+		
 		
 	}
 	
