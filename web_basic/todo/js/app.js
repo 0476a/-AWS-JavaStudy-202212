8a9836647b44d733a13defb47d3172c2babe4de4
0476a <0476a@naver.com>
2023-03-16 12:38:42 +0900

    2023.03.16(Todo 수정 및 추가)

---
+++ web_basic/todo/js/app.js
@@ -0,0 +1,7 @@
+window.onload = () => {
+    AsideEvent.getInstance().addEventShowMenuButton();
+    AsideEvent.getInstance().addEventMainChange();
+    informationEvent.getInstance().addEventPhotoChangeClick();
+    informationEvent.getInstance().addEventPhotoChange();
+
+}
